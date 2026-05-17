0d6d856ff6976c28deb52b73b80722604698a2d8
Aphelion-im <aphelion-im@protonmail.com>
2026-05-17 17:42:22 +0200

    Embedded fonts. Deleted index.css

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import AuthContextProvider from './context/AuthContext';
 import FaveContextProvider from './context/FaveContext';
 import App from './App';
-import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
